38efe9629aaf5f8657f1d9a49c53e3d877c59e89
Henry Weng <henryweng03@gmail.com>
2023-11-16 17:56:14 -0800

    add verbosity arg to training and fixed learning

--- model_simplified.py
+++ model_simplified.py
@@ -1,7 +1,7 @@
 import numpy as np
 from collections import defaultdict
 
-def discretize(value, increment=500):
+def discretize(value, increment=5000):
     return round(value / increment) * increment
 
 
@@ -16,11 +16,11 @@ class CandidateDistribution:
     
 
 # Environment parameters
-INTERVIEW_COST = 300 # cost of interviewing a candidate
+INTERVIEW_COST = 3000 # cost of interviewing a candidate
 SALARY = 150000 # salary of the candidate (we must pay this amount upon hire)
-AVG_CANDIDATE_VALUE = 130000 # average value of a candidate
-CANDIDATE_VALUE_VARIANCE = 30000 # variance of candidate value
-INTERVIEW_VARIANCE = 10000 # variance of interview score
+AVG_CANDIDATE_VALUE = 250000 # average value of a candidate
+CANDIDATE_VALUE_VARIANCE = 0 # variance of candidate value
+INTERVIEW_VARIANCE = 0 # variance of interview score
 CANDIDATE_DISTRIBUTION = CandidateDistribution(AVG_CANDIDATE_VALUE, CANDIDATE_VALUE_VARIANCE) # the candidate we are interviewing will be samples from this distribution
 
 
@@ -32,7 +32,7 @@ class HiringEnvironment:
         self.true_candidate_value = candidate_distribution.sample()
 
         self.state = [AVG_CANDIDATE_VALUE, 0]  # (avg interview score, num_interviews)
-        self.actions = ["interview", "hire", "reject"]
+        self.actions = ["hire", "reject", "interview"]
         self.reward = 0
         self.terminated = False
 
@@ -40,17 +40,18 @@ class HiringEnvironment:
         if action == "interview":
             interview_score = np.random.normal(self.true_candidate_value, self.interview_variance)
             self.state = [discretize((self.state[0] * self.state[1] + interview_score) / (self.state[1] + 1)), self.state[1] + 1]
-            self.reward = -self.interview_cost
+            self.reward -= self.interview_cost
         elif action == "hire":
-            self.reward = self.true_candidate_value - self.salary
+            self.reward = self.true_candidate_value - self.salary - self.interview_cost * self.state[1]
             self.terminated = True
         else:
+            self.reward = 0
             self.terminated = True
 
         return tuple(self.state), self.reward, self.terminated
 
     def reset(self):
-        self.state = [AVG_CANDIDATE_VALUE, 0]
+        self.state = [AVG_CANDIDATE_VALUE, 0] # prior belief about the candidate
         self.reward = 0
         self.terminated = False
         return tuple(self.state)
@@ -67,7 +68,7 @@ print(f"Next State: {next_state}, Reward: {reward}, Done: {done}")
 import random
 
 class QLearningAgent:
-    def __init__(self, env, learning_rate=0.1, discount_factor=0.9, epsilon=0.4):
+    def __init__(self, env, learning_rate=0.1, discount_factor=0.95, epsilon=0):
         self.env = env
         self.lr = learning_rate
         self.gamma = discount_factor
@@ -85,11 +86,11 @@ class QLearningAgent:
         # Implementing the Q-learning update rule
         action_index = self.env.actions.index(action)
         max_future_q = np.max(self.q_table[next_state])
-        current_q = self.q_table[state, action_index]
+        current_q = self.q_table[state][action_index]
         new_q = current_q + self.lr * (reward + self.gamma * max_future_q - current_q)
-        self.q_table[state, action_index] = new_q
-
-    def train(self, num_episodes):
+        self.q_table[state][action_index] = new_q
+    
+    def train(self, num_episodes, verbose=False):
         for episode in range(num_episodes):
             state = self.env.reset()
             done = False
@@ -99,15 +100,18 @@ class QLearningAgent:
                 next_state, reward, done = self.env.step(action)
                 self.learn(state, action, reward, next_state)
                 state = next_state
+            
+            if verbose and episode % 10000 == 0:
+                print(f"Episode: {episode}, Reward: {reward}")
 
 # Set the number of episodes for training
-num_episodes = 4
+num_episodes = 1000
 
 # Create the Q-learning agent with the environment
 ql_agent = QLearningAgent(env)
 
 # Train the agent
-ql_agent.train(num_episodes)
+ql_agent.train(num_episodes, verbose=True)
 
 # Display the final Q-table
 print(ql_agent.q_table)
